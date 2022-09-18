7fbb70db35432afdc61a7c5fac8e05e158df9fa7
ArthurVBS <arthurvini2703@gmail.com>
2022-09-18 13:02:40 -0300

    Add responsiveness & animation to the project card

--- src/components/projectCard/index.tsx
+++ src/components/projectCard/index.tsx
@@ -2,11 +2,17 @@ import React from 'react'
 import { projectType } from '../../types/projects'
 import {
   Container,
-  ContentBox,
   Image,
   ImageBox,
   Description,
   Title,
+  Header,
+  Content,
+  TextBox,
+  Tag,
+  TagBox,
+  LinkBox,
+  Link,
 } from './styles'
 
 type Props = {
@@ -14,17 +20,43 @@ type Props = {
 }
 
 const ProjectCard: React.FC<Props> = ({ project }) => {
-  console.log(project)
+  const displayTechs = () => {
+    return project.techs.map((tech, index) => <Tag key={index}>{tech}</Tag>)
+  }
+
+  const displayLinks = () => {
+    return (
+      <>
+        <Link href={project.github.repo} target="_blank">
+          <i className="fab fa-github"></i>
+        </Link>
+
+        {project.github.website.available && (
+          <Link href={project.github.website.url} target="_blank">
+            <i className="fas fa-desktop"></i>
+          </Link>
+        )}
+      </>
+    )
+  }
 
   return (
     <Container>
-      <ContentBox>
+      <Header data-aos="fade-up">
         <Title>{project.title}</Title>
-        <Description>{project.description}</Description>
-      </ContentBox>
-      <ImageBox>
-        <Image src={project.image.src} alt={project.image.alt} />
-      </ImageBox>
+      </Header>
+
+      <Content>
+        <TextBox>
+          <LinkBox data-aos="fade-down">{displayLinks()}</LinkBox>
+          <Description data-aos="fade-down">{project.description}</Description>
+          <TagBox data-aos="fade-up">{displayTechs()}</TagBox>
+        </TextBox>
+
+        <ImageBox data-aos="fade-left">
+          <Image src={project.image.src} alt={project.image.alt} />
+        </ImageBox>
+      </Content>
     </Container>
   )
 }
--- src/components/projectCard/styles.ts
+++ src/components/projectCard/styles.ts
@@ -4,31 +4,94 @@ export const Container = styled.div`
   background-color: ${props => props.theme.background};
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: stretch;
   flex-grow: 1;
+  gap: 16px;
   height: 100%;
   width: 100%;
+`
+
+export const Header = styled.header`
+  padding: 8px;
+`
+
+export const Title = styled.h1`
+  font-size: 2em;
+  letter-spacing: 0.1em;
+  text-align: center;
+
+  @media (min-width: 768px) {
+    font-size: 2.2em;
+  }
+`
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: stretch;
+  gap: 16px;
+  flex-grow: 1;
 
   @media (min-width: 768px) {
     flex-direction: row;
   }
 `
 
-export const ContentBox = styled.div`
+export const TextBox = styled.div`
   display: flex;
   flex-direction: column;
   gap: 16px;
-  flex-grow: 1;
-`
+  width: 100%;
+  padding: 8px;
 
-export const Title = styled.h1`
-  font-size: 2em;
-  text-align: center;
+  @media (min-width: 768px) {
+    width: 50%;
+  }
 `
 
 export const Description = styled.p`
   text-indent: 1em;
+  text-align: center;
+  font-size: 1.2em;
+  flex-grow: 1;
+
+  @media (min-width: 768px) {
+    font-size: 1.4em;
+  }
+`
+
+export const TagBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+`
+
+export const Tag = styled.span`
+  background-color: ${props => props.theme.primary};
+  color: ${props => props.theme.background};
+  font-weight: 700;
+  font-size: 0.8em;
+  border-radius: 4px;
+  padding: 4px 12px;
+`
+
+export const LinkBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+`
+
+export const Link = styled.a`
+  color: ${props => props.theme.primary};
+  font-size: 1.2em;
+  transition: color 0.4s, transform 0.4s;
+
+  &:hover {
+    color: ${props => props.theme.secondary};
+    transform: rotateZ(45deg);
+  }
 `
 
 export const ImageBox = styled.div`
@@ -36,9 +99,16 @@ export const ImageBox = styled.div`
   justify-content: center;
   align-items: center;
   flex-grow: 1;
+  order: -1;
   border-radius: 16px;
+  padding: 8px;
+
+  @media (min-width: 768px) {
+    order: initial;
+  }
 `
 
 export const Image = styled.img`
   max-width: 360px;
+  width: 100%;
 `
