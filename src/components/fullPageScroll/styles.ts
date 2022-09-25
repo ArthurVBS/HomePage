940fcd0600f7961c17976c2e00956ed26efca258
ArthurVBS <arthurvini2703@gmail.com>
2022-09-25 12:37:15 -0300

    Add a navbar in the tech section

--- src/components/fullPageScroll/styles.ts
+++ src/components/fullPageScroll/styles.ts
@@ -5,6 +5,7 @@ export const Container = styled.div`
   flex-grow: 1;
   width: 100%;
   height: 100%;
+  scroll-behavior: smooth;
   scroll-snap-type: y mandatory;
   border: 2px solid ${props => props.theme.primary};
   border-left: none;
@@ -16,19 +17,73 @@ export const Container = styled.div`
 `
 
 export const Section = styled.section`
+  position: relative;
   display: flex;
   flex-direction: column;
   width: 100%;
-  height: 100%;
   scroll-snap-align: start;
 
   @media (min-width: 768px) {
     flex-direction: row;
+    flex-wrap: wrap;
+    height: 100%;
+
+    &:nth-child(even) {
+      flex-direction: row-reverse;
+    }
   }
 
   &:nth-child(odd) {
     background-color: ${props => props.theme.primary};
     color: ${props => props.theme.background};
+  }
+
+  @media (min-width: 1024px) {
+    flex-wrap: nowrap;
+  }
+`
+
+export const Nav = styled.nav`
+  color: inherit;
+  width: 100%;
+  display: none;
+  padding: 16px;
+
+  @media (min-width: 768px) {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    gap: 16px;
+    order: 3;
+  }
+
+  @media (min-width: 1024px) {
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+`
+
+export const Link = styled.a`
+  color: inherit;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 4px;
+  width: 32px;
+  height: 32px;
+  border-radius: 8px;
+  transition: color 0.4s, background-color 0.4s;
+
+  &:hover {
+    background-color: ${props => props.theme.background};
+    color: ${props => props.theme.primary};
+  }
+
+  i {
+    font-size: 1.4em;
   }
 `
 
@@ -38,7 +93,6 @@ export const Text = styled.div`
   justify-content: center;
   align-items: center;
   gap: 16px;
-  height: 100%;
   width: 100%;
   padding: 16px;
 `
@@ -61,8 +115,12 @@ export const Description = styled.p`
 `
 
 export const Image = styled.img`
-  max-width: 50%;
+  max-width: 100%;
   flex-grow: 1;
   padding: 16px;
   margin: 0 auto;
+
+  @media (min-width: 768px) {
+    max-width: 50%;
+  }
 `
